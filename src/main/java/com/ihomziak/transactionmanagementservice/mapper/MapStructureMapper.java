08ec37012cf55e290a00e85686f56078a906e9db
ivanhomziak <ivan.homziak@gmail.com>
2024-10-22 14:44:42 +0200

    added mapper

--- src/main/java/com/ihomziak/transactionmanagementservice/mapper/MapStructureMapper.java
+++ src/main/java/com/ihomziak/transactionmanagementservice/mapper/MapStructureMapper.java
@@ -1,9 +1,4 @@
 package com.ihomziak.transactionmanagementservice.mapper;
 
-import com.ihomziak.transactionmanagementservice.dto.TransactionResponseDTO;
-import com.ihomziak.transactionmanagementservice.entity.Transaction;
-
 public interface MapStructureMapper {
-
-    TransactionResponseDTO mapTransactionResponseDTOFromTransaction(Transaction transaction );
 }
