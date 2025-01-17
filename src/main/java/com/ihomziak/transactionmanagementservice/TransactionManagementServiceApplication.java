96b8cd329e394d69d9ddf00c7f8be114f8eb817f
ivanhomziak <ivan.homziak@gmail.com>
2025-01-17 01:07:06 +0100

    ref

--- src/main/java/com/ihomziak/transactionmanagementservice/TransactionManagementServiceApplication.java
+++ src/main/java/com/ihomziak/transactionmanagementservice/TransactionManagementServiceApplication.java
@@ -2,10 +2,12 @@ package com.ihomziak.transactionmanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class TransactionManagementServiceApplication {
 
     public static void main(String[] args) {
